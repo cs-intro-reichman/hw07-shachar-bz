023c4c78a8e753f1a1b11c5b270965f8fb7f5e77
shachar-bz <shachar.benzur@runi.ac.il>
2024-01-31 12:15:37 +0200

    ex7

--- SpellChecker.java
+++ SpellChecker.java
@@ -3,12 +3,11 @@ public class SpellChecker {
 
 
 	public static void main(String[] args) {
-		//String word = args[0];
-		//int threshold = Integer.parseInt(args[1]);
-		//String[] dictionary = readDictionary("dictionary.txt");
-		//String correction = spellChecker(word, threshold, dictionary);
-		//System.out.println(correction);
-		System.out.println(levenshtein("SPELL", "spell"));
+		String word = args[0];
+		int threshold = Integer.parseInt(args[1]);
+		String[] dictionary = readDictionary("dictionary.txt");
+		String correction = spellChecker(word, threshold, dictionary);
+		System.out.println(correction);
 	}
 
 	public static String tail(String str) {
